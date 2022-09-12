80cef1fa645716e0c83eab224204ad3cb215779f
yansastoquegit <yasastoque@gmail.com>
2022-09-11 20:42:06 -0500

    Creamos serviciosMoviemientoDinero

--- src/main/java/com/example/HurlBiz2/servicios/ServiciosEmpleados.java
+++ src/main/java/com/example/HurlBiz2/servicios/ServiciosEmpleados.java
@@ -1,14 +1,18 @@
 package com.example.HurlBiz2.servicios;
 
 import com.example.HurlBiz2.entidades.Empleado;
+import com.example.HurlBiz2.entidades.MovimientoDinero;
 
 public class ServiciosEmpleados {
     Empleado empleado1;
+    ServiciosMovimientoDinero ServMovDine = new ServiciosMovimientoDinero();
+
 
     //Aqí se crea el constructor de ServiciosEmpleados sin parametros, pero de una vez se instancia un empleado
     //para hacer pruebas
     public ServiciosEmpleados(){
-        this.empleado1 = new Empleado("Juan", "juan@femsa.com", false, "Femsa", null);
+        MovimientoDinero MovimientoRealizado = this.ServMovDine.movimiento1;
+        this.empleado1 = new Empleado("Juan", "juan@femsa.com", false, "Femsa", MovimientoRealizado);
     }
     public Empleado getEmpleados(){
         return empleado1;
--- src/main/java/com/example/HurlBiz2/servicios/ServiciosMovimientoDinero.java
+++ src/main/java/com/example/HurlBiz2/servicios/ServiciosMovimientoDinero.java
@@ -1,4 +1,15 @@
 package com.example.HurlBiz2.servicios;
 
+import com.example.HurlBiz2.entidades.MovimientoDinero;
+
 public class ServiciosMovimientoDinero {
+    MovimientoDinero movimiento1;
+
+    //Constructor
+    public ServiciosMovimientoDinero(){
+        this.movimiento1 = new MovimientoDinero(50000, true, "Venta camisa", "Paco");
+    }
+    public MovimientoDinero getMovimientos(){
+        return this.movimiento1;
+    }
 }
